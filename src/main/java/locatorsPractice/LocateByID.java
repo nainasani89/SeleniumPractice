eb7063cee6d84c34d834ce466353b6c4feefd4fb
52084713 <52084713@METBLR52084713.ACSMET.COM>
2020-03-28 23:46:43 +0530

    Locators

---
+++ src/main/java/locatorsPractice/LocateByID.java
@@ -0,0 +1,19 @@
+package locatorsPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import com.mysql.cj.jdbc.Driver;
+
+import browser.LaunchChromeWebDriverManager;
+
+public class LocateByID extends LaunchChromeWebDriverManager{
+	
+  @Test
+  public void byID() throws InterruptedException {
+	driver.get("https://www.expedia.com/");
+	Thread.sleep(3000);
+	driver.findElement(By.id("primary-header-flight")).click();
+  }
+}
